187d5d695571898cdf0071e29dbb7722052092c9
zebra-spots <88425510+zebra-spots@users.noreply.github.com>
2023-02-23 21:52:08 +0000

    Update provine_nameID_join.sql

--- provine_nameID_join.sql
+++ provine_nameID_join.sql
@@ -1,6 +1,6 @@
 SELECT
-  first_name,
-  last_name,
-  province_name
+  patients.first_name,
+  patients.last_name,
+  province_names.province_name
 FROM patients
   JOIN province_names ON province_names.province_id = patients.province_id;
